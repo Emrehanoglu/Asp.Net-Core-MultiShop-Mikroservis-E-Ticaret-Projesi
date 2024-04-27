291b1d1db816c0dc1fc05de90a9e19333e844c71
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-04-27 11:42:42 +0300

    MultiShop #2

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
+
+public class ProductDetail
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ProductDetailId { get; set; }
+    public string ProductDescription { get; set; }
+    public string ProductInfo { get; set; }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
+
+public class ProductImages
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ProductImagesId { get; set; }
+    public string Image1 { get; set; }
+    public string Image2 { get; set; }
+    public string Image3 { get; set; }
+    public string ProductId { get; set; }
+    [BsonIgnore]
+    public Product Product { get; set; }
+}
