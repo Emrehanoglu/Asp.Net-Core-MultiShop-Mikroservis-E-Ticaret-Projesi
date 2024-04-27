c67a8a39b47df8e6376aa611a44702cbf8a6a0e7
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-04-27 12:36:52 +0300

    MultiShop #6

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/CreateProductDetailDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailDtos;
+
+public class CreateProductDetailDto
+{
+    public string ProductDescription { get; set; }
+    public string ProductInfo { get; set; }
+}
