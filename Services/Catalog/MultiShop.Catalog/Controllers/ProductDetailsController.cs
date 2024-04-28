f0fe6c4577846086fb7d9b7a0cd606ff218f2835
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-04-28 20:11:35 +0300

    MultiShop #14

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Services.ProductDetailServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductDetailsController : ControllerBase
+{
+    private readonly IProductDetailService _productDetailservice;
+
+    public ProductDetailsController(IProductDetailService ProductDetailservice)
+    {
+        _productDetailservice = ProductDetailservice;
+    }
+    [HttpGet]
+    public async Task<IActionResult> ProductDetailList()
+    {
+        var values = await _productDetailservice.GetAllProductDetailAsync();
+        return Ok(values);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductDetailById(string id)
+    {
+        var value = await _productDetailservice.GetByIdProductDetailAsync(id);
+        return Ok(value);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
+    {
+        await _productDetailservice.CreateProductDetailAsync(createProductDetailDto);
+        return Ok("Product Detail bilgisi eklendi.");
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProductDetail(string id)
+    {
+        await _productDetailservice.DeleteProductDetailAsync(id);
+        return Ok("Product Detail bilgisi silindi.");
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+    {
+        await _productDetailservice.UpdateProductDetailAsync(updateProductDetailDto);
+        return Ok("Product Detail bilgisi güncellendi.");
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductImagesController : ControllerBase
+{
+    private readonly IProductImageService _productImageservice;
+
+    public ProductImagesController(IProductImageService ProductImageservice)
+    {
+        _productImageservice = ProductImageservice;
+    }
+    [HttpGet]
+    public async Task<IActionResult> ProductImageList()
+    {
+        var values = await _productImageservice.GetAllProductImageAsync();
+        return Ok(values);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductImageById(string id)
+    {
+        var value = await _productImageservice.GetByIdProductImageAsync(id);
+        return Ok(value);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+    {
+        await _productImageservice.CreateProductImageAsync(createProductImageDto);
+        return Ok("Product Image bilgisi eklendi.");
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProductImage(string id)
+    {
+        await _productImageservice.DeleteProductImageAsync(id);
+        return Ok("Product Image bilgisi silindi.");
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+    {
+        await _productImageservice.UpdateProductImageAsync(updateProductImageDto);
+        return Ok("Product Image bilgisi güncellendi.");
+    }
+}
