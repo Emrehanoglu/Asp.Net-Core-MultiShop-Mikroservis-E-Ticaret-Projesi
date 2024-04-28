1710a771ff6eed4bd078b09bcea226aa21dadd01
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-04-28 19:44:39 +0300

    MultiShop #11

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.ProductDetailDtos;
+
+namespace MultiShop.Catalog.Services.ProductDetailServices;
+
+public interface IProductDetailService
+{
+    Task<List<ResultProductDetailDto>> GetAllProductDetailAsync();
+    Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(string id);
+    Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto);
+    Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto);
+    Task DeleteProductDetailAsync(string id);
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+
+namespace MultiShop.Catalog.Services.ProductImageServices;
+
+public interface IProductImageService
+{
+    Task<List<ResultProductImageDto>> GetAllProductImageAsync();
+    Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+    Task CreateProductImageAsync(CreateProductImageDto createProductImageDto);
+    Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto);
+    Task DeleteProductImageAsync(string id);
+}
