5054ba5a2c8f2f36da4d9b489bd67eaaf6f7b5e0
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-04-29 22:43:15 +0300

    MultiShop #18

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context;
+
+public class DapperContext:DbContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //tablomu veritabanına yansıtmak için baglantıyı burada yapacagım
+        //ilerleyen sureclerde bu baglantıyı appsettings.json 'a tasıyacagım.
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial Catalog=MultiShopDiscountDb;" +
+            "integrated security=true");
+    }
+    public DbSet<Coupon> Coupons { get; set; }
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+}
