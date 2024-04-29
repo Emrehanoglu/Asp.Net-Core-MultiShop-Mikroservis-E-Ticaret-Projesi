11d5535dd9d110de90b31264a0526c4e78a71e81
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-04-29 22:53:24 +0300

    MultiShop #19

---
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -0,0 +1,31 @@
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services;
+
+public class DiscountService : IDiscountService
+{
+    public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCouponAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ResultCouponDto>> GetAllCouponAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetByIdCouponDto> GetByIdCouponAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+    {
+        throw new NotImplementedException();
+    }
+}
