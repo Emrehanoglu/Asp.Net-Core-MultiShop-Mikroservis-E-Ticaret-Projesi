f2728fad66c9dfebe8245c60cd796e0e4fa50f21
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-09 00:15:00 +0300

    MultiShop #53

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoDetailsController : ControllerBase
+    {
+    }
+}
