05dba89e3fa78af885ba5df3eba47b2094892310
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-08 23:15:10 +0300

    MultiShop #50

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/ICargoCompanyService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract;
+
+public interface ICargoCompanyService : IGenericService<CargoCompany>
+{
+}
---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract;
+
+public interface IGenericService<T>
+{
+    void TInsert(T entity);
+    void TUpdate(T entity);
+    void TDelete(int id);
+    T TGetById(int id);
+    List<T> TGetAll();
+}
