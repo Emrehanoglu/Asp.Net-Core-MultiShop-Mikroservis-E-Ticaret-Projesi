6a9d61974aaead7f78bb083d8053f6965ac8be6e
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-11 13:44:11 +0300

    MultiShop #88

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailImageSliderComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProductDetailImageSliderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
