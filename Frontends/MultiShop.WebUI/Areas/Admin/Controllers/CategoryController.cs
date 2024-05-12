ac9a3c3e9a7e4453bbe58027f9121afc5c131b00
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-12 14:24:02 +0300

    MultiShop #97

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
