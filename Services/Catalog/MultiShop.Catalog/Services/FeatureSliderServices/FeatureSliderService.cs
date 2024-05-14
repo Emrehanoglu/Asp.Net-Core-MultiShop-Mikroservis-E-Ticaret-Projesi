28ee98e73db5de6cb431b07eee40f2a4454741a7
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-14 06:59:57 +0300

    MultiShop #115

---
+++ Services/Catalog/MultiShop.Catalog/Services/FeatureSliderServices/FeatureSliderService.cs
@@ -0,0 +1,41 @@
+﻿using MultiShop.Catalog.Dtos.FeatureSliderDtos;
+
+namespace MultiShop.Catalog.Services.FeatureSliderServices;
+
+public class FeatureSliderService : IFeatureSliderService
+{
+    public Task CreateFeatureSliderAsync(CreateFeatureSliderDto createFeatureSliderDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteFeatureSliderAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task FeatureSliderChangeStatusToFalse(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task FeatureSliderChangeStatusToTrue(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ResultFeatureSliderDto>> GetAllFeatureSliderAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetByIdFeatureSliderDto> GetByIdFeatureSliderAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateFeatureSliderAsync(UpdateFeatureSliderDto updateFeatureSliderDto)
+    {
+        throw new NotImplementedException();
+    }
+}
