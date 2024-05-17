99d370c994736e5b927580d198f547e76316ed73
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-17 21:42:01 +0300

    MultiShop #123

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+using Newtonsoft.Json;
 
-namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _SpecialOfferComponentPartial:ViewComponent
 {
-    public class _SpecialOfferComponentPartial:ViewComponent
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _SpecialOfferComponentPartial(IHttpClientFactory httpClientFactory)
     {
-        public IViewComponentResult Invoke() 
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7260/api/SpecialOffers");
+        if (responseMessage.IsSuccessStatusCode)
         {
-            return View(); 
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData);
+            return View(values);
         }
+        return View();
     }
 }
