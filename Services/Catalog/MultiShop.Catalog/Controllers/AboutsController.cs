3b563850e22854df39602f85d8fba7503eeafd1f
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-18 15:05:30 +0300

    MultiShop #138

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/AboutsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.AboutDtos;
+using MultiShop.Catalog.Services.AboutServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+[AllowAnonymous]
+[Route("api/[controller]")]
+[ApiController]
+public class AboutsController : ControllerBase
+{
+    private readonly IAboutService _aboutService;
+
+    public AboutsController(IAboutService AboutService)
+    {
+        _aboutService = AboutService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> AboutList()
+    {
+        var values = await _aboutService.GetAllAboutAsync();
+        return Ok(values);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAboutById(string id)
+    {
+        var value = await _aboutService.GetByIdAboutAsync(id);
+        return Ok(value);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+    {
+        await _aboutService.CreateAboutAsync(createAboutDto);
+        return Ok("About bilgisi eklendi.");
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAbout(string id)
+    {
+        await _aboutService.DeleteAboutAsync(id);
+        return Ok("About bilgisi silindi.");
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+    {
+        await _aboutService.UpdateAboutAsync(updateAboutDto);
+        return Ok("About bilgisi güncellendi.");
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/AboutServices/AboutService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.AboutDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.AboutServices
+{
+    public class AboutService:IAboutService
+    {
+        private readonly IMongoCollection<About> _aboutCollection;
+        private readonly IMapper _mapper;
+
+        public AboutService(IDatabaseSettings _databaseSettings, IMapper mapper)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString); //appsettings.json içerisinde tanımladığım connectionString 'i aldım.
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); //bu noktada artık veritabanına gitmiş oldum.
+            _aboutCollection = database.GetCollection<About>(_databaseSettings.AboutCollectionName); //appsettings.json içerisinde tanımladığım tablo isimlerini veritabanına yansttım.
+            _mapper = mapper;
+        }
+
+        public async Task CreateAboutAsync(CreateAboutDto createAboutDto)
+        {
+            var value = _mapper.Map<About>(createAboutDto);
+            await _aboutCollection.InsertOneAsync(value); //MongoDb 'de ekleme işlemi InsertOneAsync ile yapılır.
+        }
+
+        public async Task DeleteAboutAsync(string id)
+        {
+            await _aboutCollection.DeleteOneAsync(x => x.AboutId == id);
+        }
+
+        public async Task<List<ResultAboutDto>> GetAllAboutAsync()
+        {
+            var values = await _aboutCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultAboutDto>>(values);
+        }
+
+        public async Task<GetByIdAboutDto> GetByIdAboutAsync(string id)
+        {
+            var value = await _aboutCollection.Find<About>(x => x.AboutId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdAboutDto>(value);
+        }
+
+        public async Task UpdateAboutAsync(UpdateAboutDto updateAboutDto)
+        {
+            var values = _mapper.Map<About>(updateAboutDto);
+            //FindOneAndReplaceAsync metody ile
+            //updateAboutDto 'dan gelen AboutdId ile tablodaki kaydı bul ve values ile değiştir demiş olurum.
+            await _aboutCollection.FindOneAndReplaceAsync(x => x.AboutId == updateAboutDto.AboutId, values);
+        }
+    }
+}
