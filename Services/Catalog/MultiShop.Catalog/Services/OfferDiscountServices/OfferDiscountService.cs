e8212ef1845d108cbf863a554166b28a86d4168a
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-18 10:59:03 +0300

    MultiShop #130

---
+++ Services/Catalog/MultiShop.Catalog/Services/OfferDiscountServices/OfferDiscountService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.OfferDiscountDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.OfferDiscountServices
+{
+    public class OfferDiscountService:IOfferDiscountService
+    {
+        private readonly IMongoCollection<OfferDiscount> _offerDiscountCollection;
+        private readonly IMapper _mapper;
+
+        public OfferDiscountService(IDatabaseSettings _databaseSettings, IMapper mapper)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString); //appsettings.json içerisinde tanımladığım connectionString 'i aldım.
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); //bu noktada artık veritabanına gitmiş oldum.
+            _offerDiscountCollection = database.GetCollection<OfferDiscount>(_databaseSettings.OfferDiscountCollectionName); //appsettings.json içerisinde tanımladığım tablo isimlerini veritabanına yansttım.
+            _mapper = mapper;
+        }
+
+        public async Task CreateOfferDiscountAsync(CreateOfferDiscountDto createOfferDiscountDto)
+        {
+            var value = _mapper.Map<OfferDiscount>(createOfferDiscountDto);
+            await _offerDiscountCollection.InsertOneAsync(value); //MongoDb 'de ekleme işlemi InsertOneAsync ile yapılır.
+        }
+
+        public async Task DeleteOfferDiscountAsync(string id)
+        {
+            await _offerDiscountCollection.DeleteOneAsync(x => x.OfferDiscountId == id);
+        }
+
+        public async Task<List<ResultOfferDiscountDto>> GetAllOfferDiscountAsync()
+        {
+            var values = await _offerDiscountCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultOfferDiscountDto>>(values);
+        }
+
+        public async Task<GetByIdOfferDiscountDto> GetByIdOfferDiscountAsync(string id)
+        {
+            var value = await _offerDiscountCollection.Find<OfferDiscount>(x => x.OfferDiscountId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdOfferDiscountDto>(value);
+        }
+
+        public async Task UpdateOfferDiscountAsync(UpdateOfferDiscountDto updateOfferDiscountDto)
+        {
+            var values = _mapper.Map<OfferDiscount>(updateOfferDiscountDto);
+            //FindOneAndReplaceAsync metody ile
+            //updateOfferDiscountDto 'dan gelen OfferDiscountdId ile tablodaki kaydı bul ve values ile değiştir demiş olurum.
+            await _offerDiscountCollection.FindOneAndReplaceAsync(x => x.OfferDiscountId == updateOfferDiscountDto.OfferDiscountId, values);
+        }
+    }
+}
