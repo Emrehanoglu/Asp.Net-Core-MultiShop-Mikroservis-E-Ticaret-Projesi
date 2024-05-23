7e4fff3f58bb2c00d0882685261d7760c3633a72
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-23 21:50:32 +0300

    MultiShop #154

---
+++ IdentityServer/MultiShop.IdentityServer/Tools/GetCheckAppUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.IdentityServer.Tools;
+
+public class GetCheckAppUserViewModel
+{
+    //token 'ın içerisinde bulunacak bilgiler
+
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public string Role { get; set; }
+    public bool IsExist { get; set; }
+}
---
+++ IdentityServer/MultiShop.IdentityServer/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MultiShop.IdentityServer.Tools;
+
+public class JwtTokenGenerator
+{
+    public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel model)
+    {
+        //burada token üretilecek
+
+        var claims = new List<Claim>();
+
+        //Role kontrolü yapıyorum
+
+        if (!string.IsNullOrWhiteSpace(model.Role))
+        {
+            claims.Add(new Claim(ClaimTypes.Role, model.Role));
+        }
+        else
+        {
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, model.Id));
+        }
+
+        //Username kontrolü yapıyorum
+
+        if (!string.IsNullOrWhiteSpace(model.Username))
+        {
+            claims.Add(new Claim("Username", model.Username));
+        }
+
+        //Bu noktadan itibaren artık token oluşmaya baslıyor
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+        var signInCredential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+        JwtSecurityToken token = new JwtSecurityToken(issuer:JwtTokenDefaults.ValidIssuer,
+            audience:JwtTokenDefaults.ValidAudience, claims: claims, notBefore: DateTime.UtcNow,
+            expires: expireDate, signingCredentials:signInCredential);
+
+        //artık üretilen token 'ı geri dönebilirim.
+
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        return new TokenResponseViewModel(tokenHandler.WriteToken(token), expireDate);
+    }
+}
