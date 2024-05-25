2e920dfce8d501c09689e25c8ad6f0b8a1c14e2c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-25 11:17:36 +0300

    MultiShop #159

---
+++ Frontends/MultiShop.WebUI/Settings/ClientSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.WebUI.Settings;
+
+public class ClientSettings
+{
+    public Client MultiShopVisitorId { get; set; }
+    public Client MultiShopManagerId { get; set; }
+    public Client MultiShopAdminId { get; set; }
+}
+
+public class Client
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
