535586e25b06b2609e8c929dd6df7e1dae89281c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-05-25 11:06:43 +0300

    MultiShop #158

---
+++ Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Services.Interfaces;
+
+namespace MultiShop.WebUI.Services.Concrete;
+
+public class IdentityService : IIdentityService
+{
+    public Task<bool> SignIn(SignUpDto signUpDto)
+    {
+        //Identity/Config.cs dosyasında tanımlamış oldugum
+        //MultiShopVisitorId, MultiShopManagerId, MultiShopAdminId gibi id değerlerini tutacak.
+        
+        
+        return null;
+    }
+}
