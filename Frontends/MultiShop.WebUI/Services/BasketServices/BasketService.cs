b8f6c0d239fa94f8eef86972ea9fe104366d8d2c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-07 23:30:39 +0300

    MultiShop #202

---
+++ Frontends/MultiShop.WebUI/Services/BasketServices/BasketService.cs
@@ -0,0 +1,22 @@
+﻿using MultiShop.DtoLayer.BasketDtos;
+
+namespace MultiShop.WebUI.Services.BasketServices
+{
+    public class BasketService : IBasketService
+    {
+        public Task DeleteBasket(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketTotalDto> GetBasket(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/BasketServices/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using MultiShop.DtoLayer.BasketDtos;
+
+namespace MultiShop.WebUI.Services.BasketServices;
+
+public interface IBasketService
+{
+    Task<BasketTotalDto> GetBasket(string userId);
+    Task SaveBasket(BasketTotalDto basketTotalDto);
+    Task DeleteBasket(string userId);
+}
