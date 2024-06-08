f286fb53d11b91e69768dcde9f1726e4d9bbd8df
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-08 13:39:05 +0300

    MultiShop #214

---
+++ Frontends/MultiShop.WebUI/Controllers/DiscountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers;
+
+public class DiscountController : Controller
+{
+    [HttpGet]
+    public PartialViewResult ConfirmDiscountCoupon()
+    {
+        return PartialView();
+    }
+    [HttpPost]
+    public PartialViewResult ConfirmDiscountCoupon(string code)
+    {
+        return PartialView();
+    }
+}
