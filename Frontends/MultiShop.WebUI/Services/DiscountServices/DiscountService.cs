8b27646addea42f8e9fc3802372338ac6d5b64c6
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-08 12:23:12 +0300

    MultiShop #212

--- Frontends/MultiShop.WebUI/Services/DiscountServices/DiscountService.cs
+++ Frontends/MultiShop.WebUI/Services/DiscountServices/DiscountService.cs
@@ -1,12 +1,18 @@
 ﻿using MultiShop.DtoLayer.DiscountDtos;
 
-namespace MultiShop.WebUI.Services.DiscountServices
+namespace MultiShop.WebUI.Services.DiscountServices;
+
+public class DiscountService : IDiscountService
 {
-    public class DiscountService : IDiscountService
+    private readonly HttpClient _httpClient;
+    public DiscountService(HttpClient httpClient)
     {
-        public Task<GetDiscountCodeDetailByCodeDto> GetDiscountCode(string code)
-        {
-            throw new NotImplementedException();
-        }
+        _httpClient = httpClient;
+    }
+    public async Task<GetDiscountCodeDetailByCodeDto> GetDiscountCode(string code)
+    {
+        var responseMessage = await _httpClient.GetAsync("discounts/GetDiscountDetailByCode/" + code);
+        var values = await responseMessage.Content.ReadFromJsonAsync<GetDiscountCodeDetailByCodeDto>();
+        return values;
     }
 }
