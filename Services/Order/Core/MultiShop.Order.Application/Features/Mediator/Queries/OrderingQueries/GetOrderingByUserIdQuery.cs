ebcf13f98486288f76851f4199d642862a5c2e59
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-11 21:30:15 +0300

    MultiShop #231

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByUserIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+public class GetOrderingByUserIdQuery : IRequest<List<GetOrderingByUserIdQuery>>
+{
+}
