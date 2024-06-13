b056f5a97f2e9b08bd1a06fc224332cb2d2a749d
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-13 19:30:21 +0300

    MultiShop #240

--- Services/Message/MultiShop.Message/DAL/Context/MessageContext.cs
+++ Services/Message/MultiShop.Message/DAL/Context/MessageContext.cs
@@ -1,6 +1,14 @@
-﻿namespace MultiShop.Message.DAL.Context
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Message.DAL.Entities;
+
+namespace MultiShop.Message.DAL.Context
 {
-    public class MessageContext
+    public class MessageContext : DbContext
     {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UserMessage> UserMessages { get; set; }
     }
 }
