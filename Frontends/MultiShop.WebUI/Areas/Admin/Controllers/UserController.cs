11bbc75d7fb11b753b8907bdb1c997095f6a0f8c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-15 20:35:54 +0300

    MultiShop #255

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/UserController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.CargoServices.CargoCustomerServices;
 using MultiShop.WebUI.Services.UserIdentityServices;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers;
@@ -8,16 +9,25 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers;
 public class UserController : Controller
 {
     private readonly IUserIdentityService _userIdentityService;
+    private readonly ICargoCustomerService _cargoCustomerService;
 
-    public UserController(IUserIdentityService userIdentityService)
+    public UserController(IUserIdentityService userIdentityService, ICargoCustomerService cargoCustomerService)
     {
         _userIdentityService = userIdentityService;
+        _cargoCustomerService = cargoCustomerService;
     }
 
     [Route("UserList")]
     public async Task<IActionResult> UserList()
     {
         var values = await _userIdentityService.GetAllUserListtAsync();
+        return View(values);
+    }
+
+    [Route("UserAddressInfo/{id}")]
+    public async Task<IActionResult> UserAddressInfo(string id)
+    {
+        var values = await _cargoCustomerService.GetByIdCargoCustomerInfoAsync(id);
         return View(values);
     }
 }
---
+++ Frontends/MultiShop.WebUI/Services/CargoServices/CargoCustomerServices/CargoCustomerService.cs
@@ -0,0 +1,23 @@
+﻿using MultiShop.DtoLayer.CargoDtos.CargoCustomerDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Services.CargoServices.CargoCustomerServices
+{
+    public class CargoCustomerService : ICargoCustomerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CargoCustomerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<GetCargoCustomerByIdDto> GetByIdCargoCustomerInfoAsync(string id)
+        {
+            var responseMessage = await _httpClient.GetAsync("CargoCustomers/GetCargoCustomerById/" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<GetCargoCustomerByIdDto>(jsonData);
+            return values;
+        }
+    }
+}
