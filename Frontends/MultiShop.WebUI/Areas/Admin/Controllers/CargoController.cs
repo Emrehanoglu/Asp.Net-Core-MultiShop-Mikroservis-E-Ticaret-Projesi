b8fab48be9449c04f6216a66cc328bee9a4a483c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-15 13:21:46 +0300

    MultiShop #250

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CargoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/Cargo")]
+public class CargoController : Controller
+{
+    [Route("CargoList")]
+    public IActionResult CargoList()
+    {
+        return View();
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/CargoServices/CargoCompanyServices/CargoCompanyService.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.DtoLayer.CargoDtos.CargoCompanyDtos;
+
+namespace MultiShop.WebUI.Services.CargoServices.CargoCompanyServices
+{
+    public class CargoCompanyService : ICargoCompanyService
+    {
+        public Task CreateCargoCompanyAsync(CreateCargoCompanyDto createCargoCompanyDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCargoCompanyAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCargoCompanyDto>> GetAllCargoCompanyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateCargoCompanyDto> GetByIdCargoCompanyAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCargoCompanyAsync(UpdateCargoCompanyDto updateCargoCompanyDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
