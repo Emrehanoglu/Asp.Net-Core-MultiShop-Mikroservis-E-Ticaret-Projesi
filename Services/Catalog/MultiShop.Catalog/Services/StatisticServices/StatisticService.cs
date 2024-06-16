c160f15e7dcb130f881e2fc4aece75f3345da973
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-16 10:31:23 +0300

    MultiShop #256

---
+++ Services/Catalog/MultiShop.Catalog/Services/StatisticServices/StatisticService.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.StatisticServices;
+
+public class StatisticService : IStatisticService
+{
+    private readonly IMongoCollection<Product> _productCollection;
+    private readonly IMongoCollection<Category> _categoryCollection;
+    private readonly IMongoCollection<Brand> _brandCollection;
+
+    public StatisticService(IDatabaseSettings _databaseSettings)
+    {
+        var client = new MongoClient(_databaseSettings.ConnectionString);
+        var database = client.GetDatabase(_databaseSettings.DatabaseName);
+        _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+        _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+        _brandCollection = database.GetCollection<Brand>(_databaseSettings.BrandCollectionName);
+    }
+}
