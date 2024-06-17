eb418d63c7e667e7964c6fc1113e6cf2e0defdd6
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-17 14:08:05 +0300

    MultiShop #271

--- Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed((host) => true).AllowCredentials();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
