e508229294eaad9bd2f2a2c2deab9df200ac82fc
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-17 16:29:42 +0300

    MultiShop #274

--- Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Services/SignalRCommentServices/SignalRCommentService.cs
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Services/SignalRCommentServices/SignalRCommentService.cs
@@ -2,14 +2,17 @@
 {
     public class SignalRCommentService : ISignalRCommentService
     {
-        private readonly HttpClient _httpClient;
-        public SignalRCommentService(HttpClient httpClient)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SignalRCommentService(IHttpClientFactory httpClientFactory)
         {
-            _httpClient = httpClient;
+            _httpClientFactory = httpClientFactory;
         }
+
         public async Task<int> GetTotalCommentCount()
         {
-            var responseMessage = await _httpClient.GetAsync("comments/GetTotalCommentCount");
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7265/api/CommentStatistics");
             var value = await responseMessage.Content.ReadFromJsonAsync<int>();
             return value;
         }
