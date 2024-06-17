cdc47ca43e89f139f1e97e8ee54b513ae93ee84b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-17 10:57:26 +0300

    MultiShop #268

--- Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Services.MessageServices;
+using MultiShop.WebUI.Services.StatisticServices.CommentStatisticServices;
 
 namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
 {
     public class _AdminLayoutHeaderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IMessageService _messageService;
+        private readonly IUserService _userService;
+        private readonly ICommentStatisticService _commentStatisticService;
+
+        public _AdminLayoutHeaderComponentPartial(IMessageService messageService, IUserService userService, ICommentStatisticService commentStatisticService)
         {
+            _messageService = messageService;
+            _userService = userService;
+            _commentStatisticService = commentStatisticService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //gelen mesaj sayısı
+            var user = await _userService.GetUserInfo();
+            int messageCount = await _messageService.GetTotalMessageCountByReceiverId(user.Id);
+            ViewBag.messageCount = messageCount;
+
+            //toplam yorum sayısı
+            int totalCommentCount = await _commentStatisticService.GetTotalCommentCount();
+            ViewBag.totalCommentCount = totalCommentCount;
+
             return View();
         }
     }
---
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Hubs/SignalRHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MultiShop.SignalRRealTimeApi.Hubs;
+
+public class SignalRHub : Hub
+{
+
+}
