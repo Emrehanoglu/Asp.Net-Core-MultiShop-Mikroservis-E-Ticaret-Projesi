cb6a22b4d9b0c6d278dabc19ae6abc19fad7a56d
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-21 19:02:43 +0300

    MultiShop #275

---
+++ Services/Images/MultiShop.Images/Controllers/GoogleDriveImageUploadController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Images.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GoogleDriveImageUploadController : ControllerBase
+{
+}
