3fa8470fef8516ac91c857f0e3da8dd7878e912f
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-25 10:31:44 +0300

    MultiShop #277

---
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessagesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.RabbitMQMessageApi.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CreateMessage()
+        {
+            return Ok("Mesajınız Kuyruga Alınmıstır.");
+        }
+    }
+}
