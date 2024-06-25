64e3677e8a47621e9d634d594c99816823fee1d0
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-25 21:45:05 +0300

    MultiShop #279

---
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUI.Models;
+using Newtonsoft.Json;
+
+namespace MultiShop.RapidApiWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> WeatherDetail()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/istanbul"),
+                Headers =
+    {
+        { "x-rapidapi-key", "94d000cff7msh79345f9040df6aep14c0c6jsna5ed5618645e" },
+        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<WeatherViewModel.Rootobject>(body);
+                ViewBag.cityTemp = values.data.temp;
+                return View();
+            }
+        }
+    }
+}
