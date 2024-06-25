3b9e8c1b810df8e335a2ff5d5c911f0817d2926c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-25 22:14:22 +0300

    MultiShop #280

--- RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
@@ -29,5 +29,30 @@ namespace MultiShop.RapidApiWebUI.Controllers
                 return View();
             }
         }
+
+        public async Task<IActionResult> Exchange()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-time-series?from_symbol=USD&to_symbol=TRY&period=1D&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "94d000cff7msh79345f9040df6aep14c0c6jsna5ed5618645e" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.exchange = values.data.exchange_rate;
+
+                return View();
+            }
+        }
     }
 }
---
+++ RapidApi/MultiShop.RapidApiWebUI/Models/ExchangeViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace MultiShop.RapidApiWebUI.Models
+{
+    public class ExchangeViewModel
+    {
+
+        public class Rootobject
+        {
+            public string status { get; set; }
+            public string request_id { get; set; }
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            public string from_symbol { get; set; }
+            public string to_symbol { get; set; }
+            public string type { get; set; }
+            public float exchange_rate { get; set; }
+            public float previous_close { get; set; }
+            public string last_update_utc { get; set; }
+            public Time_Series time_series { get; set; }
+            public int utc_offset_sec { get; set; }
+            public int interval_sec { get; set; }
+            public string period { get; set; }
+        }
+
+        public class Time_Series
+        {
+            
+        }
+    }
+}
